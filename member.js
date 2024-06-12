3f320e444501ece483c2cd284c61819193e2930f
Miguel Labajos <81683463+milabajos10@users.noreply.github.com>
2024-06-12 11:43:53 +0000

    Second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMembers() {
+  let members = [
+    { name: 'John', age: 25, skills: ['HTML', 'CSS', 'JS'] },
+    { name: 'Jack', age: 30, skills: ['HTML', 'CSS', 'JS', 'React'] },
+    { name: 'Sara', age: 22, skills: ['HTML', 'JS'] },
+    { name: 'Lily', age: 27, skills: ['HTML', 'CSS', 'JS', 'React'] },
+    { name: 'Mary', age: 35, skills: ['HTML', 'CSS', 'JS', 'React', 'Angular'] },
+  ];
+
+  return members;
+}
